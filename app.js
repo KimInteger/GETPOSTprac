6748b6ceb663d556d663d98313de9665b1f6ea3b
KimInteger <kjs20151240@gmail.com>
2024-05-30 09:51:24 +0900

    GET메서드일때 req.url을 조건으로 if문 틀을 작성하고 /일 경우의 실행문 중 fs.readFile의 err까지만 작성함

--- app.js
+++ app.js
@@ -10,7 +10,16 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req,res)=>{
   if (req.method === 'GET') {
+    if (req.url === '/') {
+      fs.readFile('./public/index.html', (err,data)=>{
+        if (err) {
+          res.writeHead(500,{"Content-Type" : "text/plain; charset=UTF-8"});
+          res.end("서버 자체에서 에러가 발생했습니다.");
+        } else {
 
+        }
+      });
+    }
   } else if (req.method === 'POST') {
 
   } else {
