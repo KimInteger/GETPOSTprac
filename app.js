d86d72bd69407fc75e6c0ca3196f7db28bb074e3
KimInteger <kjs20151240@gmail.com>
2024-05-30 09:56:40 +0900

    GET요청으로 받아온 정보를 txt파일로 생성하는데 성공함 받아오고 생성까지 완료

--- app.js
+++ app.js
@@ -22,8 +22,29 @@ const server = http.createServer((req,res)=>{
       });
     } else if (req.url.startsWith('/get')) {
       let getData = req.url.split('?')[1];
+      
       let decodeData = qs.decode(getData);
-      console.log(decodeData);
+
+      let jsonData = {
+        name : decodeData.name,
+        hobby : decodeData.hobby
+      }
+      
+      fs.writeFile(`./public/${decodeData.name}.txt`,JSON.stringify(jsonData,null,2),(err)=>{
+        if (err) {
+          console.error(err);
+        } else {
+          fs.readFile('./public/index.html', (err,data)=>{
+            if (err) {
+              res.writeHead(500,{"Content-Type" : "text/plain; charset=UTF-8"});
+              res.end("서버 자체에서 에러가 발생했습니다.");
+            } else {
+              res.writeHead(200,{"Content-Type" : "text/html; charset=UTF-8"});
+              res.end(data);
+            }
+          });
+        }
+      });
     } else {
 
     }
